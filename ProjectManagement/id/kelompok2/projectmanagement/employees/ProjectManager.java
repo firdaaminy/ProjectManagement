8a043f6cc936b222c884ca0038527a700d672059
Abbi PC <Abbi PC@ABBI>
2016-03-12 23:09:35 +0700

    ProjectManager Tasks

--- ProjectManagement/id/kelompok2/projectmanagement/employees/ProjectManager.java
+++ ProjectManagement/id/kelompok2/projectmanagement/employees/ProjectManager.java
@@ -2,31 +2,44 @@ package id.kelompok2.projectmanagement.employees;
 
 import id.kelompok2.projectmanagement.projects.Project;
 
-public class ProjectManager extends Person {
-	
-	Project[] projects;
-	int currentProject;
-	
-	public ProjectManager(String name, long id) {
-		super(name, id);
-		currentProject = 0;
-	}
-	
-	public ProjectManager(String name, long id, double salary) {
-		super(name, id, salary);
-		currentProject = 0;
-	}
-	
-	public Project[] getProjects() {
-		return projects;
-	}
-	
-	public Project getProject(int id) {
-		return projects[id];
-	}
-	
-	public void createProject() {
-		projects[currentProject] = new Project();
-	}
-	
+public class ProjectManager extends Person
+{
+    private Project[] projects;
+    private int numOfProjects;
+    private double salary;
+
+public ProjectManager(String name, long id, double salary){
+    super(name, id, salary);
+}
+
+public Project[] getProjects(){
+        return projects;
+    }
+
+public Project getProject(int id){
+    return projects[id];
+}
+
+public void createProject(){
+    
+}
+public assignProject(long id){
+    
+}
+public unassignProject(long id){
+}
+
+public assignTask(Programmer programmer, Project project, Task task){
+}
+
+public deleteProject(int pid){
+}
+
+public double getSalary(){
+    return salary;
+}
+
+public void setSalary(double Salary){
+    
+}
 }
---
+++ ProjectManagement/id/kelompok2/projectmanagement/projects/Task.java
@@ -0,0 +1,33 @@
+package id.kelompok2.projectmanagement.projects;
+
+public class Task
+{
+    String taskName;
+    int taskId;
+    boolean done;
+
+public Task(String taskName, int taskId){
+    this.taskName = taskName;
+    this.taskId = taskId;
+}
+public String getName() {
+    return taskName;
+}
+
+public int getId(){
+    return taskId;
+}
+public void setName(){
+    this.taskName = taskName;
+}
+public void setId(){
+    this.taskId = taskId;
+}
+public boolean getStatuS(){
+    return done;
+}
+public void setStatus(boolean done){
+    this.done = done;
+}
+        
+}
